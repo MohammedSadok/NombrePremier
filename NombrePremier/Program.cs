e910462a45a65c90d6147479efe04efab7ed95ae
Mohammed <mohammed.sadok.2003@gmail.com>
2021-10-10 07:57:43 +0100

    Add project files.

---
+++ NombrePremier/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NombrePremier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entrer un nombre : ");
+            var nbrAsString = Console.ReadLine();
+            int nbr;
+            while (!int.TryParse(nbrAsString, out nbr))
+            {
+                Console.WriteLine("Ce n'est pas un nombre entier!");
+                nbrAsString = Console.ReadLine();
+            }
+
+            bool test = false;
+            for (int i = 2; i <= nbr / 2 && test == false; i++)
+            {
+                if (nbr % i == 0)
+                    test = true;
+            }
+            if (test)
+                Console.WriteLine(nbr + " est un nombre premier");
+            else
+                Console.WriteLine(nbr + " n'est pas un nombre premier");
+        }
+    }
+}
